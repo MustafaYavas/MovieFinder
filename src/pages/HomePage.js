b11a21a102661afde633bd06b667018bb403c607
UB4ME <mustafayavas40@gmail.com>
2022-02-25 19:00:26 +0300

    Add useEffect to main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,15 @@
 import Movies from "../components/movies/Movies";
 
+import { useEffect } from "react";
+import { movieActions } from "../store/movie-slice";
+import { useDispatch } from "react-redux";
+
 const HomePage = () => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(movieActions.changeSearchKey(''))
+    }, [dispatch])
 
     return (
         <Movies />
