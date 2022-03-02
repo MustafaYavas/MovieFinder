c98eb77a12a3545fe5e5a7c17cbd40175105bd83
UB4ME <mustafayavas40@gmail.com>
2022-03-02 12:46:38 +0300

    Add close button to search bar

--- src/components/UI/SearchBar.js
+++ src/components/UI/SearchBar.js
@@ -1,36 +1,32 @@
 import styles from './SearchBar.module.css';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { movieActions } from '../../store/movie-slice';
 
 const SearchBar = () => {
     const inputRef = useRef();
     const dispatch = useDispatch();
+    const [showCloseButton, setShowCloseButton] = useState(false);
 
     const inputChangeHandler = (e) => {
         const enteredText = inputRef.current.value;
         dispatch(movieActions.changeSearchKey(enteredText));
+        if(!enteredText) return setShowCloseButton(false);
+        else return setShowCloseButton(true);
     }
 
+    const clearTextHandler = () => {
+        inputRef.current.value = '';
+        dispatch(movieActions.changeSearchKey(''))
+    }
 
     return (
-        <>
-            {
-                /*
-                <form className='position-relative'>
-                    <div className={`mb-3 w-50 position-absolute top-50 start-50 translate-middle  ${styles['input-container']}`}>
-                        <input autoFocus type="text" onChange={inputChangeHandler} ref={inputRef} className={`form-control bg-dark fw-bold text-light ${styles.input}`} id="movie" aria-describedby="movie" placeholder='Search Movie'/>
-                        <i className='fa-solid fa-xmark text-light '></i>
-                    </div>
-                </form>
-                */
-            }
-            
+        <>        
             <form className={`d-flex justify-content-center align-items-center ${styles['input-margin']}`}>
-                <div className=' w-50 d-flex justify-content-center align-items-center'>
+                <div className=' w-50 position-relative'>
                     <input autoFocus type="text" onChange={inputChangeHandler} ref={inputRef} className={`form-control bg-dark fw-bold text-light ${styles.input}`} id="movie" aria-describedby="movie" placeholder='Search Movie'/>
-                    <i className={`fa-solid fa-xmark text-light ${styles['icon-position']}`}></i>
+                    {showCloseButton && <i onClick={clearTextHandler} className={`fa-solid fa-xmark text-light ${styles['icon-position']}`}></i>}
                 </div>
             </form>
         </>
