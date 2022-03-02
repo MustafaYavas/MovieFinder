3166f3e5ab73649cba16de1ada38655dfa4e1f7e
UB4ME <mustafayavas40@gmail.com>
2022-03-02 13:27:43 +0300

    Change search states

--- src/components/UI/SearchBar.js
+++ src/components/UI/SearchBar.js
@@ -1,31 +1,35 @@
 import styles from './SearchBar.module.css';
 
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { movieActions } from '../../store/movie-slice';
 
 const SearchBar = () => {
-    const inputRef = useRef();
     const dispatch = useDispatch();
+    const [inputText, setInputText] = useState('');
     const [showCloseButton, setShowCloseButton] = useState(false);
 
     const inputChangeHandler = (e) => {
-        const enteredText = inputRef.current.value;
+        const enteredText = e.target.value;
+        setInputText(enteredText);
         dispatch(movieActions.changeSearchKey(enteredText));
-        if(!enteredText) return setShowCloseButton(false);
-        else return setShowCloseButton(true);
     }
 
     const clearTextHandler = () => {
-        inputRef.current.value = '';
+        setInputText('')
         dispatch(movieActions.changeSearchKey(''))
     }
+
+    useEffect(() => {
+        if(!inputText) setShowCloseButton(false);
+        else setShowCloseButton(true);
+    }, [inputText])
 
     return (
         <>        
             <form className={`d-flex justify-content-center align-items-center ${styles['input-margin']}`}>
                 <div className=' w-50 position-relative'>
-                    <input autoFocus type="text" onChange={inputChangeHandler} ref={inputRef} className={`form-control bg-dark fw-bold text-light ${styles.input}`} id="movie" aria-describedby="movie" placeholder='Search Movie'/>
+                    <input autoFocus type="text" onChange={inputChangeHandler} value={inputText} className={`form-control bg-dark fw-bold text-light ${styles.input}`} id="movie" aria-describedby="movie" placeholder='Search Movie'/>
                     {showCloseButton && <i onClick={clearTextHandler} className={`fa-solid fa-xmark text-light ${styles['icon-position']}`}></i>}
                 </div>
             </form>
